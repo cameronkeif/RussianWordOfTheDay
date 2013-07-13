5529fe094a607d7bf0f4a1913f9a173a59489612
Cameron Keif <cameron.keif@gmail.com>
2013-07-13 01:42:33 -0400

    Randomly parses one line of definitions.xml, and updates the text views appropriately.
    **NOTE: tested using the 3 longest values, государственном, as... as possible (as in 'as soon as possible'), and cardinal pronoun. The XML file is very messy, however, and will need to be tested again after cleaning it up!

--- src/edu/msu/keifcame/russianwordoftheday/RussianWOTDWidgetProvider.java
+++ src/edu/msu/keifcame/russianwordoftheday/RussianWOTDWidgetProvider.java
@@ -1,30 +1,79 @@
 package edu.msu.keifcame.russianwordoftheday;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
 public class RussianWOTDWidgetProvider extends AppWidgetProvider {
-
+	String mRussianWord   = "";
+	String mDefintion     = "";
+	String mPartsOfSpeech = "";
+	
 	public RussianWOTDWidgetProvider() {
 		// TODO Auto-generated constructor stub
 	}
 
 	 public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		 // Do stuff
 		 final int N = appWidgetIds.length;
 
+		 Random rand = new Random();
+		 int wordNumber = rand.nextInt( 1000 );
+		 
         // Perform this loop procedure for each App Widget that belongs to this provider
         for (int i=0; i<N; i++) {
             int appWidgetId = appWidgetIds[i];
             
-         // Get the layout for the App Widget and attach an on-click listener
-        // to the button
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-
-        // Tell the AppWidgetManager to perform an update on the current app widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+           // Update text views
+           RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+           
+           parseXML( context, wordNumber );
+           
+           views.setTextViewText( R.id.russianWord, mRussianWord );
+           views.setTextViewText( R.id.englishDefinition, mDefintion );
+           views.setTextViewText( R.id.partOfSpeech, mPartsOfSpeech );
+   
+           // Tell the AppWidgetManager to perform an update on the current app widget
+           appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+	 }
+	 
+	 /**
+      * Parse the definitions xml file
+      * @param wordNumber [0, 999] represents the line of the xml file
+      */
+	 private void parseXML( Context context, int wordNumber ) {
+		  try {
+			 InputStream istr = context.getAssets().open("definitions.xml");
+			 XmlPullParserFactory factory = XmlPullParserFactory.newInstance(); 
+			 factory.setNamespaceAware(true); 
+			 XmlPullParser xrp = factory.newPullParser(); 
+			 xrp.setInput(istr, "UTF-8"); 
+			 
+			 int eventType = xrp.getEventType();
+			 int index = 0;
+			 while (eventType != XmlPullParser.END_DOCUMENT && index <= wordNumber ) {
+		          if(eventType == XmlPullParser.START_TAG) {
+		              System.out.println("Start tag "+xrp.getName());
+		              mDefintion = xrp.getAttributeValue( null, "definition" );
+		              mPartsOfSpeech = xrp.getAttributeValue( null, "parts" );
+		              mRussianWord = xrp.getAttributeValue( null, "word" );
+	                  index++;
+		          }
+		          eventType = xrp.next();
+			 }
+		  } catch ( IOException e ) {
+			  e.printStackTrace();
+			} catch ( XmlPullParserException e ) {
+			 e.printStackTrace();
+		}
 	 }
 }
