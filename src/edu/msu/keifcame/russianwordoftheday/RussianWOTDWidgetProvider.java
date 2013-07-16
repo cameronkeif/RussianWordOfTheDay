41de7beb0a1e10b84ed354e5a3e39de8bedc731c
Cameron Keif <cameron.keif@gmail.com>
2013-07-15 22:49:38 -0400

    lint-- (imports)

--- src/edu/msu/keifcame/russianwordoftheday/RussianWOTDWidgetProvider.java
+++ src/edu/msu/keifcame/russianwordoftheday/RussianWOTDWidgetProvider.java
@@ -11,7 +11,6 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
