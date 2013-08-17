a6fedf44895716743a113119bee013bc39c91e6e
Cameron Keif <cameron.keif@gmail.com>
2013-08-16 22:34:20 -0400

    Update tabs

--- src/edu/msu/keifcame/russianwordoftheday/FragmentTabs.java
+++ src/edu/msu/keifcame/russianwordoftheday/FragmentTabs.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class FragmentTabs extends FragmentActivity {
    private FragmentTabHost mTabHost;
    
+   @SuppressWarnings ( "deprecation")
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
@@ -16,12 +18,18 @@ public class FragmentTabs extends FragmentActivity {
       mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
       mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
       
-      mTabHost.addTab(mTabHost.newTabSpec("asdf").setIndicator("asdf"),
+      mTabHost.addTab(mTabHost.newTabSpec("Current").setIndicator("Current"),
             WordFragment.class, null);
-      mTabHost.addTab(mTabHost.newTabSpec("simple").setIndicator("Simple"),
+      mTabHost.addTab(mTabHost.newTabSpec("Blocked").setIndicator("Blocked"),
             BlockedWordsFragment.class, null);
-      mTabHost.addTab(mTabHost.newTabSpec("contacts").setIndicator("Contacts"),
+      mTabHost.addTab(mTabHost.newTabSpec("Recent").setIndicator("Recent"),
             RecentWordsFragment.class, null);
+      
+      for ( int i = 0; i < mTabHost.getTabWidget().getChildCount(); i++) {
+         mTabHost.getTabWidget().getChildAt( i ).setBackgroundDrawable( getResources().getDrawable( R.drawable.tab_selector ) );
+         TextView tv = (TextView) mTabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+         tv.setTextColor( getResources().getColor( R.color.off_white ) );
+      }
    }
 	
    @Override
